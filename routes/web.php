5ea9912e2077657c7b7ae0c892bb500decb6c5d6
naosuf <fukasawa.naoki01@gmail.com>
2022-03-20 23:20:24 +0900

    for 1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/posts', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 
 Route::get('/', 'PostController@index');
 
 Route::get('/posts/{post}', 'PostController@show');
+
