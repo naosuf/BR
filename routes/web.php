6c64d457213d799b33e63beb1980ec83a835fb24
naosuf <fukasawa.naoki01@gmail.com>
2022-03-21 12:52:52 +0900

    08-5 complete

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::post('posts', 'PostController@store');
-
 Route::get('/', 'PostController@index');
-
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
 Route::get('/posts/{post}', 'PostController@show');
 
