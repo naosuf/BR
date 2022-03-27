4674d2845823a135894cdefaede110848ba57297
naosuf <fukasawa.naoki01@gmail.com>
2022-03-27 11:58:48 +0900

    curriculum cplt

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,20 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Category;
 use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-      return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+      $client = new \GuzzleHttp\Client();
+      
+      $url = 'https://teratail.com/api/v1/questions';
+      
+      $response = $client->request(
+        'GET',
+        $url,
+        ['Bearer' => config('services.teratail.token')]
+      );
+      
+      $questions = json_decode($response->getBody(), true);
+      
+      return view('posts/index')->with([
+        'posts' => $post->getPaginateByLimit(),
+        'questions' => $questions['questions'],
+        ]);
     } 
     public function show(Post $post){
       return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create(){
-      return view('/posts/create');
+    public function create(Category $category){
+      return view('/posts/create')->with(['categories' => $category->get()]);
     }
     
     public function store(PostRequest $request, Post $post){
@@ -40,6 +56,8 @@ class PostController extends Controller
       $post->delete();
       return redirect('/');
     }
+    
+    
 
 
 }
